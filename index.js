b31cec14116c049ee8874e61266e3d5f36ce3053
lapeko <lapeko88@gmail.com>
2023-12-17 03:14:13 +0100

    parse post body

--- index.js
+++ index.js
@@ -3,11 +3,23 @@ const http = require("http");
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  res.setHeader("X-Powered-By", "Blabla");
+  const auth = req.headers["authorization"];
+  console.log("Authorization: ", auth);
+
+  res.writeHead(200, {
+    "Content-Type": "text/html",
+    "X-Powered-By": "Blabla",
+  });
+
+  const chunks = [];
+  req
+    .on("data", (chunk) => chunks.push(chunk))
+    .on("end", () => {
+      console.log(Buffer.concat(chunks).toString());
+    });
+
   res.write("<h1>Header</h1>");
   res.write("<p>Some text</p>");
-  res.statusCode = 200;
   res.end();
 });
 
